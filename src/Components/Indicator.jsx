d9fda29f74a343d5e1cf9abc22903500d4faad0a
coderbekhub <coderbek@mail.ru>
2023-08-15 09:38:35 +0500

    indicator done

--- src/Components/Indicator.jsx
+++ src/Components/Indicator.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import CountUp from 'react-countup';
 
 export default function Indicator() {
   return (
-    <div>Indicator</div>
+    <section className='indicatorSection'>
+      {/* <div className='blur'></div> */}
+      <div className="container">
+        <div className='pTop'>
+          <div className='indicatorContent text-center row'>
+            <div className='col-12 col-md-3 '>
+              <h1><CountUp start={0} end={22} duration={2} delay={0} />+</h1>
+              <h2>Филиалы в городе</h2>
+            </div>
+            <div className='col-12 col-md-3 '>
+              <h1><CountUp start={0} end={278} duration={2} delay={0} />+</h1>
+              <h2>Лояльные клиенты</h2>
+            </div>
+            <div className='col-12 col-md-3 '>
+              <h1><CountUp start={0} end={800} duration={2} delay={0} />+</h1>
+              <h2>Услуги, предоставляемыу</h2>
+            </div>
+            <div className='col-12 col-md-3 '>
+              <h1><CountUp start={0} end={32} duration={2} delay={0} />+</h1>
+              <h2>Лучшие эксперты</h2>
+            </div>
+          </div>
+        </div>
+      </div>  
+    </section>
   )
 }
