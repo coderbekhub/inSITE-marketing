b38ac67363c57239fd348f3c5b9a826049ea2fa0
coderbekhub <coderbek@mail.ru>
2023-08-22 15:55:57 +0500

    ...

--- src/Components/ServicePrice.jsx
+++ src/Components/ServicePrice.jsx
@@ -4,9 +4,10 @@ export default function ServicePrice() {
   return (
     <section className='priceSection'>
       <div className="container">
-        <div className='row priceContent'>
-          <div className='priceCard col-md-3 col-12 card'>
-            <h2>Veb dizayn</h2>
+        <h2 className='priceTitle'>Narxlar</h2>
+        <div className='priceContent row justify-content-center'>
+          <div className='priceCard card'>
+            <h2>Landing page</h2>
             <span>Одностраничный сайт</span>
             <div className='priceHr'></div>
             <ul>
@@ -16,11 +17,11 @@ export default function ServicePrice() {
               <li>Brand book</li>
             </ul>
             <h3>300$</h3>
-            <a href="#">Bepul Konsultatsiya</a>
+            <a href="tel:+998500105610">Bepul Konsultatsiya</a>
           </div>
 
-          <div className='priceCard col-md-3 col-12 card'>
-            <h2>Veb dizayn</h2>
+          <div className='priceCard card'>
+            <h2>Katalog sayt</h2>
             <span>Одностраничный сайт</span>
             <div className='priceHr'></div>
             <ul>
@@ -30,11 +31,11 @@ export default function ServicePrice() {
               <li>Brand book</li>
             </ul>
             <h3>300$</h3>
-            <a href="#">Bepul Konsultatsiya</a>
+            <a href="tel:+998500105610">Bepul Konsultatsiya</a>
           </div>
 
-          <div className='priceCard col-md-3 col-12 card'>
-            <h2>Veb dizayn</h2>
+          <div className='priceCard card'>
+            <h2>Internet magazin</h2>
             <span>Одностраничный сайт</span>
             <div className='priceHr'></div>
             <ul>
@@ -44,23 +45,69 @@ export default function ServicePrice() {
               <li>Brand book</li>
             </ul>
             <h3>300$</h3>
-            <a href="#">Bepul Konsultatsiya</a>
-          </div>
-
-          <div className='priceCard col-md-3 col-12 card'>
-            <h2>Veb dizayn</h2>
-            <span>Одностраничный сайт</span>
-            <div className='priceHr'></div>
-            <ul>
-              <li>Logotip</li>
-              <li>Landing page </li>
-              <li>Brand book</li>
-              <li>Brand book</li>
-            </ul>
-            <h3>300$</h3>
-            <a href="#">Bepul Konsultatsiya</a>
+            <a href="tel:+998500105610">Bepul Konsultatsiya</a>
           </div>
         </div>
+
+        <div className='priceContent row justify-content-center'>
+          <div className='priceCard card'>
+            <h2>Google</h2>
+            <span>Одностраничный сайт</span>
+            <div className='priceHr'></div>
+            <ul>
+              <li>Анализ ниши и конкурентов</li>
+              <li>Анализ сайта + рекомендации </li>
+              <li>Оптимизация ставок и бюджета</li>
+              <li>Подбор ключевых слов</li>
+              <li>Оптимизация ставок и бюджета</li>
+              <li>Подключение метрики</li>
+              <li>Еженедельный отчет</li>
+              <li>Срок настройки: 7-10 дней</li>
+            </ul>
+            <h3>100$</h3>
+            <span className='priceMonth'>в месяц</span>
+            <a href="tel:+998500105610">Bepul Konsultatsiya</a>
+          </div>
+
+          <div className='priceCard card'>
+            <h2>Yandex</h2>
+            <span>Одностраничный сайт</span>
+            <div className='priceHr'></div>
+            <ul>
+              <li>Анализ ниши и конкурентов</li>
+              <li>Анализ сайта + рекомендации </li>
+              <li>Оптимизация ставок и бюджета</li>
+              <li>Подбор ключевых слов</li>
+              <li>Оптимизация ставок и бюджета</li>
+              <li>Подключение метрики</li>
+              <li>Еженедельный отчет</li>
+              <li>Срок настройки: 7-10 дней</li>
+            </ul>
+            <h3>100$</h3>
+            <span className='priceMonth'>в месяц</span>
+            <a href="tel:+998500105610">Bepul Konsultatsiya</a>
+          </div>
+
+          <div className='priceCard card'>
+            <h2>Google + Yandex</h2>
+            <span>Одностраничный сайт</span>
+            <div className='priceHr'></div>
+            <ul>
+              <li>Анализ ниши и конкурентов</li>
+              <li>Анализ сайта + рекомендации </li>
+              <li>Оптимизация ставок и бюджета</li>
+              <li>Подбор ключевых слов</li>
+              <li>Оптимизация ставок и бюджета</li>
+              <li>Подключение метрики</li>
+              <li>Еженедельный отчет</li>
+              <li>Срок настройки: 7-10 дней</li>
+            </ul>
+            <h3>100$</h3>
+            <span className='priceMonth'>в месяц</span>
+            <a href="tel:+998500105610">Bepul Konsultatsiya</a>
+          </div>
+        </div>
+
       </div>
     </section>
   )
