ff34177f4f81aaa62ee236d894a046b3c36e4243
coderbekhub <coderbek@mail.ru>
2023-08-28 12:46:26 +0500

    ...

--- src/Components/Advantages.jsx
+++ src/Components/Advantages.jsx
@@ -5,38 +5,38 @@ export default function Advantages() {
     <section className='advantageSection'>
       <div className="container">
         <h2 className='advantagesTitle'>Bizning afzaliklarimiz</h2>
-        <div className='advantageContent row justify-content-center'>
-            <div className='col-md-4 col-12'>
-              <i class='bx bxs-group' ></i>
-              <h3>Tajribali jamoa a’zolari</h3>
-              <p>Biznimg jamoamiz azolari o’z ishlarining mutaxasislari bo’lib o’z sohalari bo’yicha 1,5 yildan ortiq tajribaga ega va ma’sulyatlidirlar</p>
-            </div>
-            <div className='col-md-4 col-12'>
-              <i class='bx bx-support' ></i>
-              <h3>Texnik qo’llab quvatlash</h3>
-              <p>Loyiha yakunlangandan so’ng 3 oy davomida hodimlarimiz tamonidin nazorat qilinib turiladi</p>
-            </div>
-            <div className='col-md-4 col-12'>
-              <i class='bx bx-purchase-tag' ></i>
-              <h3>Qulay narx va sifat</h3>
-              <p>Mangosteen Group o’z mijozlariga qulay narxlar bilan birgalikda a’lo darajadagi sifatni ham ulashadi</p>
-            </div>
+        <div className='advantageContent row justify-content-between'>
+          <div className='col-md-4 col-12'>
+            <i class='bx bxs-group' ></i>
+            <h3>Tajribali jamoa a’zolari</h3>
+            <p>Biznimg jamoamiz azolari o’z ishlarining mutaxasislari bo’lib o’z sohalari bo’yicha 1,5 yildan ortiq tajribaga ega va ma’sulyatlidirlar</p>
+          </div>
+          <div className='col-md-4 col-12'>
+            <i class='bx bx-support' ></i>
+            <h3>Texnik qo’llab quvatlash</h3>
+            <p>Loyiha yakunlangandan so’ng 3 oy davomida hodimlarimiz tamonidin nazorat qilinib turiladi</p>
+          </div>
+          <div className='col-md-4 col-12'>
+            <i class='bx bx-purchase-tag' ></i>
+            <h3>Qulay narx va sifat</h3>
+            <p>Mangosteen Group o’z mijozlariga qulay narxlar bilan birgalikda a’lo darajadagi sifatni ham ulashadi</p>
+          </div>
 
-            <div className='col-md-4 col-12'>
-              <i class='bx bxs-check-shield'></i>
-              <h3>Kafolatlangan xizmat</h3>
-              <p>Biz xizmatimiz kelishilinganidek bo’lishiga 100% kafolat beramiz va belgilangan mudatda kelishilingan xolda topshiramiz</p>
-            </div>
-            <div className='col-md-4 col-12'>
-              <i class='bx bxs-castle'></i>
-              <h3>Profisional dizayn</h3>
-              <p>Biz dizaynga prodisional tarzda yondashamiz dizayn yagona boladi xechqanday nusxa emas</p>
-            </div>
-            <div className='col-md-4 col-12'>
-              <i class='bx bxl-creative-commons' ></i>
-              <h3>Creative g'oyalar</h3>
-              <p>Bizning jamoa hamkorlik jarayonida yangi g'oyalar va takliflar bilan bo'lishadi</p>
-            </div>
+          <div className='col-md-4 col-12'>
+            <i class='bx bxs-check-shield'></i>
+            <h3>Kafolatlangan xizmat</h3>
+            <p>Biz xizmatimiz kelishilinganidek bo’lishiga 100% kafolat beramiz va belgilangan mudatda kelishilingan xolda topshiramiz</p>
+          </div>
+          <div className='col-md-4 col-12'>
+            <i class='bx bxs-castle'></i>
+            <h3>Profisional dizayn</h3>
+            <p>Biz dizaynga prodisional tarzda yondashamiz dizayn yagona boladi xechqanday nusxa emas</p>
+          </div>
+          <div className='col-md-4 col-12'>
+            <i class='bx bxl-creative-commons' ></i>
+            <h3>Creative g'oyalar</h3>
+            <p>Bizning jamoa hamkorlik jarayonida yangi g'oyalar va takliflar bilan bo'lishadi</p>
+          </div>
         </div>
       </div>
     </section>
--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -5,31 +5,26 @@ export default function Service() {
     <section className='serviceSection'>
       <div className="container">
         <h2 className='servicesTitle'>Xizmatlarimiz</h2>
-
-        <div className='row servicesContent'>
+        <div className='row servicesContent justify-content-center'>
           <div className='col-md-6 col-12'>
-            <div>
-              <i class='bx bx-globe' ></i>
-              <h3>Veb saytlar</h3>
-              <p>Veb-saytingizni biz bilan yarating. Biz turli xildagi saytlarni siz istaganingizdek qilib yaratib beramiz.</p>
-            </div>
-            <div>
-              <i class='bx bx-line-chart'></i>
-              <h3>Google reklama (SEO)</h3>
-              <p>Google va Yandex qidiruv tizimida saytingizni profisional reklama qilib beramiz </p>
-            </div>
+            <i class='bx bx-globe' ></i>
+            <h3>Veb saytlar</h3>
+            <p>Veb-saytingizni biz bilan yarating. Biz turli xildagi saytlarni siz istaganingizdek qilib yaratib beramiz.</p>
           </div>
           <div className='col-md-6 col-12'>
-            <div>
-                <i class='bx bxl-telegram'></i>
-                <h3>Telegram bot</h3>
-                <p>Telegram botingizni biz bilan yarating. Biz turli xildagi botlarni siz istaganingizdek qilib yaratib beramiz.</p>
-              </div>
-              <div className='serviceCard'>
-                <i class='bx bxl-reddit'></i>
-                <h3>Veb dizayn</h3>
-                <p>Web Dizayn | Sayt Uchun Ijodiy va Betakror G'oyalar</p>
-              </div>
+            <i class='bx bx-line-chart'></i>
+            <h3>Google reklama (SEO)</h3>
+            <p>Google va Yandex qidiruv tizimida saytingizni profisional reklama qilib beramiz </p>
+          </div>
+          <div className='col-md-6 col-12'>
+            <i class='bx bxl-telegram'></i>
+            <h3>Telegram bot</h3>
+            <p>Telegram botingizni biz bilan yarating. Biz turli xildagi botlarni siz istaganingizdek qilib yaratib beramiz.</p>
+          </div>
+          <div className='col-md-6 col-12'>
+            <i class='bx bxl-reddit'></i>
+            <h3>Veb dizayn</h3>
+            <p>Web Dizayn | Sayt Uchun Ijodiy va Betakror G'oyalar</p>
           </div>
         </div>
       </div>
