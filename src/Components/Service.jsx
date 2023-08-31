caa743e87407557606e4be9d0165e2fcf7be73d0
coderbekhub <coderbek@mail.ru>
2023-08-31 17:56:59 +0500

    ...

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -7,22 +7,22 @@ export default function Service() {
       <div className="container">
         <h2 className='servicesTitle'>{getText('serviceTitle')}</h2>
         <div className='row servicesContent justify-content-center'>
-          <div  className='col-md-6 col-12'>
+          <div  className='col-xl-6 col-md-6 col-12'>
             <i class='bx bx-globe'></i>
             <h3>{getText('serviceType')}</h3>
             <p>{getText('serviceText')}</p>
           </div>
-          <div className='col-md-6 col-12'>
+          <div className='test col-xl-6 col-md-6 col-12'>
             <i class='bx bx-line-chart'></i>
             <h3>{getText('serviceType2')}</h3>
             <p>{getText('serviceText2')} </p>
           </div>
-          <div className='col-md-6 col-12'>
+          <div className='col-xl-6 col-md-6 col-12'>
             <i class='bx bxl-telegram'></i>
             <h3>{getText('serviceType3')}</h3>
             <p>{getText('serviceText3')}</p>
           </div>
-          <div className='col-md-6 col-12'>
+          <div className='test col-xl-6 col-md-6 col-12'>
             <i class='bx bxl-reddit'></i>
             <h3>{getText('serviceText4')}</h3>
             <p>{getText('serviceText4')}</p>
